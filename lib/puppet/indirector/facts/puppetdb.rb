dd9d2fabde4f007b38e48d79ab751647e5c97332
Marius Rieder <marius.rieder@durchmesser.ch>
2017-07-05 13:36:46 +0200

    Ensure puppetdb-terminus 2.3.x is compatiblle with Puppet5.

--- lib/puppet/indirector/facts/puppetdb.rb
+++ lib/puppet/indirector/facts/puppetdb.rb
@@ -21,7 +21,7 @@ class Puppet::Node::Facts::Puppetdb < Puppet::Indirector::REST
       payload = profile("Encode facts command submission payload",
                         [:puppetdb, :facts, :encode]) do
         facts = request.instance.dup
-        facts.values = facts.strip_internal.dup
+        facts.values = facts.values.dup
 
         if ! Puppet::Util::Puppetdb.puppet3compat? || Puppet[:trusted_node_data]
           facts.values[:trusted] = get_trusted_info(request.node)
